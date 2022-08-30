3eeb2d53d9d52d7e7fcd93e998fe1d96e3c72eb3
Sara Mediouni <81996794+Sara-Mediouni@users.noreply.github.com>
2022-08-30 02:09:47 +0100

    gt

--- src/components/Events/Evènements.js
+++ src/components/Events/Evènements.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './eventspage.css';
 import img1 from '../../images/Sports_1.jpg';
 import img2 from '../../images/Sports_2.jpg';
@@ -12,7 +12,34 @@ import img9 from '../../images/Sports_9.jpg';
 import img10 from '../../images/Sports_10.jpg';
 import img11 from '../../images/Sports_11.jpg';
 import img12 from '../../images/Sports_12.jpg';
+import axios from 'axios';
 export default function Events() {
+ const [news, setnews] = React.useState([{id: 0}]);
+  const [events, setevents] = React.useState([{id: 0}]);
+  const show=()=>{
+    axios.get("http://localhost:3000/api/event/")
+    .then(response => {
+      const events = response.data;
+      setevents(events)
+      console.log(events)
+  
+  })};
+  
+ 
+  const shownews=()=>{
+    axios.get("http://localhost:3000/api/news/")
+    .then(response => {
+      const news = response.data;
+      setnews(news)
+      console.log(news)
+  
+  })};
+  useEffect(()=>{
+    show()
+    shownews()
+
+  }
+  ,[]);
   return (
     <div className="content-wrapper">
           <div className="container">
@@ -28,11 +55,12 @@ export default function Events() {
                   </div>
                   <div className="row">
                     <div className="col-lg-8">
-                      <div className="row">
+                    {  events.map((e,key)=>{return(
+                      <div className="row" key={key}>
                         <div className="col-sm-4 grid-margin">
                           <div className="rotate-img">
                             <img
-                              src={img1}
+                              src={"http://localhost:3000/uploadsevent/"+e.Image}
                               alt="banner"
                               className="img-fluid"
                             />
@@ -40,165 +68,39 @@ export default function Events() {
                         </div>
                         <div className="col-sm-8 grid-margin">
                           <h2 className="font-weight-600 mb-2">
-                            No charges over 2017 battle bus cases
+                           {e.Titre}
                           </h2>
-                          <p className="fs-13 text-muted mb-0">
-                            <span className="mr-2">Photo </span>10 Minutes ago
-                          </p>
+                         
                           <p className="fs-15">
-                            Lorem Ipsum has been the industry's standard dummy
-                            text ever since the 1500s, when an unknown printer
-                            took a galley of type and scrambled it to make a
-                            type specimen book.
+                          {e.Description}
                           </p>
                         </div>
                       </div>
-                      <div className="row">
-                        <div className="col-sm-4 grid-margin">
-                          <div className="rotate-img">
-                            <img
-                              src={img2}
-                              alt="banner"
-                              className="img-fluid"
-                            />
-                          </div>
-                        </div>
-                        <div className="col-sm-8 grid-margin">
-                          <h2 className="font-weight-600 mb-2">
-                            No charges over 2017 battle bus cases
-                          </h2>
-                          <p className="fs-13 text-muted mb-0">
-                            <span className="mr-2">Photo </span>10 Minutes ago
-                          </p>
-                          <p className="fs-15">
-                            Lorem Ipsum has been the industry's standard dummy
-                            text ever since the 1500s, when an unknown printer
-                            took a galley of type and scrambled it to make a
-                            type specimen book.
-                          </p>
-                        </div>
-                      </div>
-                      <div className="row">
-                        <div className="col-sm-4 grid-margin">
-                          <div className="rotate-img">
-                            <img
-                              src={img3}
-                              alt="banner"
-                              className="img-fluid"
-                            />
-                          </div>
-                        </div>
-                        <div className="col-sm-8 grid-margin">
-                          <h2 className="font-weight-600 mb-2">
-                            Kaine: Trump Jr. may have committed treason
-                          </h2>
-                          <p className="fs-13 text-muted mb-0">
-                            <span className="mr-2">Photo </span>10 Minutes ago
-                          </p>
-                          <p className="fs-15">
-                            Lorem Ipsum has been the industry's standard dummy
-                            text ever since the 1500s, when an unknown printer
-                            took a galley of type and scrambled it to make a
-                            type specimen book.
-                          </p>
-                        </div>
-                      </div>
-                      <div className="row">
-                        <div className="col-sm-4 grid-margin">
-                          <div className="rotate-img">
-                            <img
-                              src={img4}
-                              alt="banner"
-                              className="img-fluid"
-                            />
-                          </div>
-                        </div>
-                        <div className="col-sm-8 grid-margin">
-                          <h2 className="font-weight-600 mb-2">
-                            South Korea’s Moon Jae-in sworn in vowing to address
-                          </h2>
-                          <p className="fs-13 text-muted mb-0">
-                            <span className="mr-2">Photo </span>10 Minutes ago
-                          </p>
-                          <p className="fs-15">
-                            Lorem Ipsum has been the industry's standard dummy
-                            text ever since the 1500s, when an unknown printer
-                            took a galley of type and scrambled it to make a
-                            type specimen book.
-                          </p>
-                        </div>
-                      </div>
-                      <div className="row">
-                        <div className="col-sm-4 grid-margin">
-                          <div className="rotate-img">
-                            <img
-                              src={img5}
-                              alt="banner"
-                              className="img-fluid"
-                            />
-                          </div>
-                        </div>
-                        <div className="col-sm-8 grid-margin">
-                          <h2 className="font-weight-600 mb-2">
-                            No charges over 2017 battle bus cases
-                          </h2>
-                          <p className="fs-13 text-muted mb-0">
-                            <span className="mr-2">Photo </span>10 Minutes ago
-                          </p>
-                          <p className="fs-15">
-                            Lorem Ipsum has been the industry's standard dummy
-                            text ever since the 1500s, when an unknown printer
-                            took a galley of type and scrambled it to make a
-                            type specimen book.
-                          </p>
-                        </div>
-                      </div>
-                      <div className="row">
-                        <div className="col-sm-4 grid-margin">
-                          <div className="rotate-img">
-                            <img
-                              src={img6}
-                              alt="banner"
-                              className="img-fluid"
-                            />
-                          </div>
-                        </div>
-                        <div className="col-sm-8 grid-margin">
-                          <h2 className="font-weight-600 mb-2">
-                            Kaine: Trump Jr. may have committed treason
-                          </h2>
-                          <p className="fs-13 text-muted mb-0">
-                            <span className="mr-2">Photo </span>10 Minutes ago
-                          </p>
-                          <p className="fs-15">
-                            Lorem Ipsum has been the industry's standard dummy
-                            text ever since the 1500s, when an unknown printer
-                            took a galley of type and scrambled it to make a
-                            type specimen book.
-                          </p>
-                        </div>
-                      </div>
+                    )})}
+                      
                     </div>
                     <div className="latest col-lg-4">
                       <h2 className="mb-4 font-weight-600">
                         Actualités
                       </h2>
-                      <div className="row">
+                      {
+                        news.map((n,key)=>{
+                          return( <div className="row" key={key}>
                         <div className="col-sm-12">
                           <div className="border-bottom pb-4 pt-4">
                             <div className="row">
                               <div className="col-sm-8">
                                 <h5 className="font-weight-600 mb-1">
-                                  Ways to stay social online while in self..
+                                  {n.Titre}
                                 </h5>
                                 <p className="fs-13 text-muted mb-0">
-                                  <span className="mr-2">Photo </span>10 Minutes ago
+                                  <span className="mr-2">{n.Description}</span>
                                 </p>
                               </div>
                               <div className="col-sm-4">
                                 <div className="rotate-img">
                                   <img
-                                    src={img7}
+                                    src={"http://localhost:3000/uploadsnews/"+n.Image}
                                     alt="banner"
                                     className="img-fluid"
                                   />
@@ -207,107 +109,12 @@ export default function Events() {
                             </div>
                           </div>
                         </div>
-                      </div>
-                      <div className="row">
-                        <div className="col-sm-12">
-                          <div className="border-bottom pb-4 pt-4">
-                            <div className="row">
-                              <div className="col-sm-8">
-                                <h5 className="font-weight-600 mb-1">
-                                  Premier League players join charity..
-                                </h5>
-                                <p className="fs-13 text-muted mb-0">
-                                  <span className="mr-2">Photo </span>10 Minutes ago
-                                </p>
-                              </div>
-                              <div className="col-sm-4">
-                                <div className="rotate-img">
-                                  <img
-                                    src={img7}
-                                    alt="banner"
-                                    className="img-fluid"
-                                  />
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="row">
-                        <div className="col-sm-12">
-                          <div className="pt-4">
-                            <div className="row">
-                              <div className="col-sm-8">
-                                <h5 className="font-weight-600 mb-1">
-                                  UK Athletics board changed stance on..
-                                </h5>
-                                <p className="fs-13 text-muted mb-0">
-                                  <span className="mr-2">Photo </span>10 Minutes ago
-                                </p>
-                              </div>
-                              <div className="col-sm-4">
-                                <div className="rotate-img">
-                                  <img
-                                    src={img8}
-                                    alt="banner"
-                                    className="img-fluid"
-                                  />
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="trending">
-                        <h2 className="mb-4  font-weight-600">
-                          Trending
-                        </h2>
-                        <div className="mb-4">
-                          <div className="rotate-img">
-                            <img
-                              src={img9}
-                              alt="banner"
-                              className="img-fluid"
-                            />
-                          </div>
-                          <h3 className="mt-3 font-weight-600">
-                            Virus Kills Member Of Advising Iran’s Supreme
-                          </h3>
-                          <p className="fs-13 text-muted mb-0">
-                            <span className="mr-2">Photo </span>10 Minutes ago
-                          </p>
-                        </div>
-                        <div className="mb-4">
-                          <div className="rotate-img">
-                            <img
-                              src={img10}
-                              alt="banner"
-                              className="img-fluid"
-                            />
-                          </div>
-                          <h3 className="mt-3 font-weight-600">
-                            Virus Kills Member Of Advising Iran’s Supreme
-                          </h3>
-                          <p className="fs-13 text-muted mb-0">
-                            <span className="mr-2">Photo </span>10 Minutes ago
-                          </p>
-                        </div>
-                        <div className="mb-4">
-                          <div className="rotate-img">
-                            <img
-                              src={img11}
-                              alt="banner"
-                              className="img-fluid"
-                            />
-                          </div>
-                          <h3 className="mt-3 font-weight-600">
-                            Virus Kills Member Of Advising Iran’s Supreme
-                          </h3>
-                          <p className="fs-13 text-muted mb-0">
-                            <span className="mr-2">Photo </span>10 Minutes ago
-                          </p>
-                        </div>
-                      </div>
+                      </div>)
+                        })
+                      }
+                     
+                      
+                      
                     </div>
                   </div>
                 </div>
--- src/components/HomeSections/Actualites.js
+++ src/components/HomeSections/Actualites.js
@@ -8,31 +8,35 @@ import { useEffect } from 'react';
 import {useAnimation} from 'framer-motion'
 import axios from 'axios';
 export default function Actualites() {
-  const [events, setEvents] = React.useState([{id: 0}]);
+  const [news1, setnews1] = React.useState([{id: 0}]);
+  const [news2, setnews2] = React.useState([{id: 0}]);
   const show=()=>{
-    axios.get("http://localhost:3000/api/event/")
+    axios.get("http://localhost:3000/api/news/")
     .then(response => {
-      const events = response.data;
-      setEvents(events)
-      console.log(events)
+      const news1 = response.data[0];
+      setnews1(news1)
+   
+      const news2 = response.data[1];
+      setnews1(news2)
+     
   })};
   const{ref, inView}=useInView({
     threshold:0.2
   });
   const animation=useAnimation();
   useEffect(()=>{
+    show()
 if (inView){
   animation.start({
     x:0,
     transition:{
-      type:'spring',duration:1,bounce:0.3
+      type:'spring',duration:1,bounce:0.8
     }
   });
 }if (!inView){
   animation.start({x:'-100vw'})
 }
-  },
-  show()
+  }
   ,[inView]);
   return (
     <section ref={ref} className='news col-md-12 col-xs-10 col-xs-offset-1'>
@@ -41,19 +45,20 @@ if (inView){
 animate={animation}  
 className='article'>
   
-<img src={"http://localhost:3000/uploadsevent/"+events[0].Image} className="image col-md-5 col-xs-3 col-xs-offset-1"/>
-<p className='title'><span>{events[0].Titre}</span> {events[0].Description}</p>
+<img src={"http://localhost:3000/uploadsnews/"+news1.Image} className="image col-md-5 col-xs-3 col-xs-offset-1"/>
+<p className='title'><span>{news1.Titre}</span> {news1.Description}</p>
 
 </motion.div>
 <motion.div 
 animate={animation} 
  className='article2'>
-<img src="" className="image2"/>
-<p className='title2'><span>{events[1].Titre}</span> 
-{events[1].Description}</p>
+<img src={"http://localhost:3000/uploadsnews/"+news2.Image} className="image2"/>
+<p className='title2'><span>{news2.Titre}</span> 
+{news2.Description}
+</p>
 
 </motion.div>
-<button className="addbutton">voir plus</button>
+<button href="/events" className="addbutton">voir plus</button>
     </section>
   )
 }
